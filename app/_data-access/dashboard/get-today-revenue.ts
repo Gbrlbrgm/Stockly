da5f71060f332847abac037829ddb274c8772b29
Gbrlbrgm <gabriel_bergamo@outlook.com>
2025-11-19 21:08:40 -0300

    feat: add total in stock card component with streaming

--- app/_data-access/dashboard/get-today-revenue.ts
+++ app/_data-access/dashboard/get-today-revenue.ts
@@ -1,15 +1,19 @@
-import "server-only";
-
 import { db } from "@/app/_lib/prisma";
 
-export const getTotalRevenue = async (): Promise<number> => {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const totalRevenueQuery = `
-    SELECT SUM("SaleProduct"."unitPrice" * "SaleProduct"."quantity") as "totalRevenue"
+export const getTodayRevenue = async (): Promise<number> => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const todayRevenueQuery = `
+    SELECT SUM("SaleProduct"."unitPrice" * "SaleProduct"."quantity") as "todayRevenue"
     FROM "SaleProduct"
-    JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id";
+    JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id"
+    WHERE "Sale"."date" >= $1 AND "Sale"."date" <= $2;
   `;
-  const totalRevenue =
-    await db.$queryRawUnsafe<{ totalRevenue: number }[]>(totalRevenueQuery);
-  return totalRevenue[0].totalRevenue;
+  const startOfDay = new Date(new Date().setHours(0, 0, 0, 0));
+  const endOfDay = new Date(new Date().setHours(23, 59, 59, 999));
+  const todayRevenue = await db.$queryRawUnsafe<{ todayRevenue: number }[]>(
+    todayRevenueQuery,
+    startOfDay,
+    endOfDay,
+  );
+  return todayRevenue[0].todayRevenue;
 };
